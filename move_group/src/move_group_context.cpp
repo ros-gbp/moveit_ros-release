38b2cb50bfd60fa37752f9fc3e8dccc775900da6
Ioan Sucan <isucan@willowgarage.com>
2013-07-12 16:57:44 +0200

    Rebase from 'upstream'

--- move_group/src/move_group_context.cpp
+++ move_group/src/move_group_context.cpp
@@ -41,26 +41,26 @@
 #include <moveit/plan_execution/plan_with_sensing.h>
 
 move_group::MoveGroupContext::MoveGroupContext(const planning_scene_monitor::PlanningSceneMonitorPtr &planning_scene_monitor,
-					       bool allow_trajectory_execution, bool debug) : 
+                           bool allow_trajectory_execution, bool debug) :
   planning_scene_monitor_(planning_scene_monitor),
   allow_trajectory_execution_(allow_trajectory_execution),
   debug_(debug)
 {
   planning_pipeline_.reset(new planning_pipeline::PlanningPipeline(planning_scene_monitor_->getRobotModel()));
-  
+
   if (allow_trajectory_execution_)
-  {  
+  {
     trajectory_execution_manager_.reset(new trajectory_execution_manager::TrajectoryExecutionManager(planning_scene_monitor_->getRobotModel()));
     plan_execution_.reset(new plan_execution::PlanExecution(planning_scene_monitor_, trajectory_execution_manager_));
     plan_with_sensing_.reset(new plan_execution::PlanWithSensing(trajectory_execution_manager_));
     if (debug)
       plan_with_sensing_->displayCostSources(true);
   }
-    
+
   // configure the planning pipeline
   planning_pipeline_->displayComputedMotionPlans(true);
   planning_pipeline_->checkSolutionPaths(true);
-  
+
   if (debug_)
     planning_pipeline_->publishReceivedRequests(true);
 }
@@ -76,7 +76,7 @@ move_group::MoveGroupContext::~MoveGroupContext()
 
 bool move_group::MoveGroupContext::status() const
 {
-  const planning_interface::PlannerPtr &planner_interface = planning_pipeline_->getPlannerInterface();
+  const planning_interface::PlannerManagerPtr &planner_interface = planning_pipeline_->getPlannerManager();
   if (planner_interface)
   {
     ROS_INFO_STREAM("MoveGroup context using planning plugin " << planning_pipeline_->getPlannerPluginName());
